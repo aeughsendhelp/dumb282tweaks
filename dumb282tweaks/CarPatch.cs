189abd1ef9e31cc7485181b25dc886bde9bb5d87
EvilPunkin <82129788+EvilPunkin@users.noreply.github.com>
2023-12-05 18:40:51 -0800

    things work now, not all of them though

--- dumb282tweaks/CarPatch.cs
+++ dumb282tweaks/CarPatch.cs
@@ -14,20 +14,25 @@ namespace dumb282tweaks;
 class CarPatch {
 	static void Postfix(ref TrainCar __instance) {
 		if(__instance != null && __instance.carType == TrainCarType.LocoSteamHeavy) {
+			bool chonk = false;
 			// This is a terrible way to get the mesh of the locomotive since a simple gameobject reordering will break it, but it's alright for now. If something breaks, just note that this is likely the reason
 			Transform originalBody = __instance.transform.GetChild(5).transform.GetChild(0).transform.GetChild(0);
-			//Transform originalSmokeBoxDoor = __instance.transform.GetChild(5).transform.GetChild(0).transform.GetChild(7);
+			Transform originalSmokeBoxDoor = __instance.transform.GetChild(5).transform.GetChild(0).transform.GetChild(7);
 			//Transform originalHeadlights = __instance.transform.GetChild(5).transform.GetChild(13);
-
-
 			// This is a better way of getting the mesh, but I don't feel like trying to understand transform.Find, so I'll come back this later. I think the directory with slashes needs to be exactly parents and children, no skipping levels
 			//Transform s282Mesh = carInstance.transform.Find("LocoS282A_Body/s282_locomotive_body");
 			Material s282Mat = originalBody.GetComponent<MeshRenderer>().material;
 			originalBody.gameObject.SetActive(false);
-			//originalSmokeBoxDoor.gameObject.SetActive(false);
+			originalSmokeBoxDoor.gameObject.SetActive(false);
 			//originalHeadlights.gameObject.SetActive(false);
 
 			GameObject baseReal = InstantiateLoadedObject(baseLoad, s282Mat, __instance.transform);
+
+			if(Main.Settings.boilerType == Settings.BoilerType.Chonky) {
+				chonk = true;
+			} else {
+				chonk = false;
+			}
 
 			// Boiler
 			switch(Main.Settings.boilerType) {
@@ -36,6 +41,10 @@ class CarPatch {
 					break;
 				case Settings.BoilerType.Streamlined:
 					GameObject streamlineBoiler = InstantiateLoadedObject(streamlineBoilerLoad, s282Mat, __instance.transform);
+					break;
+				case Settings.BoilerType.Chonky:
+					GameObject chonkyBoiler = InstantiateLoadedObject(chonkyBoilerLoad, s282Mat, __instance.transform);
+					chonkyBoiler.transform.localPosition = new Vector3(0, 0.2f, objOffset);
 					break;
 			}
 			// Cab
@@ -64,15 +73,20 @@ class CarPatch {
 					GameObject streamlinedCowCatcher = InstantiateLoadedObject(streamlinedCowCatcherLoad, s282Mat, __instance.transform);
 					break;
 			}
-			//// Smoke Box Door
-			//switch(Main.Settings.smokeBoxDoorType) {
-			//	case Settings.SmokeBoxDoorType.Default:
-			//		GameObject defaultSmokeBoxDoor = InstantiateLoadedObject(defaultSmokeBoxDoorLoad, s282Mat, __instance.transform);
-			//		break;
-			//	case Settings.SmokeBoxDoorType.Center:
-			//		GameObject frontSmokeBoxDoor = InstantiateLoadedObject(frontSmokeBoxDoorLoad, s282Mat, __instance.transform);
-			//		break;
-			//}
+			// Smoke Box Door
+			switch(Main.Settings.smokeBoxDoorType) {
+				case Settings.SmokeBoxDoorType.Default:
+					GameObject defaultSmokeBoxDoor = InstantiateLoadedObject(defaultSmokeBoxDoorLoad, s282Mat, __instance.transform);
+					defaultSmokeBoxDoor.transform.localPosition = new Vector3(0, 2.60208f, 5.69122f + objOffset);
+					if(chonk) {
+						defaultSmokeBoxDoor.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+					}
+					break;
+				case Settings.SmokeBoxDoorType.Center:
+					//GameObject centerSmokeBoxDoor = InstantiateLoadedObject(centerSmokeBoxDoorLoad, s282Mat, __instance.transform);
+					//centerSmokeBoxDoor.transform.localPosition = new Vector3(0, 2.60208f, 5.69122f + objOffset);
+					break;
+			}
 			// Smoke Deflectors
 			switch(Main.Settings.smokeDeflectorType) {
 				case Settings.SmokeDeflectorType.None:
